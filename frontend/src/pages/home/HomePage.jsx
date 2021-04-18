4738e269dd799b7b7958e0ec448fdc5efda232d4
Rabunnisa <rabia.zabiulla@gmail.com>
2021-04-19 00:02:44 +0200

    Home css styling added

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -6,10 +6,12 @@ export default function HomePage() {
     <div className="card">
       <div className="card-body">
         <h4 className="card-title"></h4>
+          <h2>Importance of Agriculture</h2>
         <p>
           The global population is increasing rapidly. And we need more food and other products to meet up this huge demand. Traditional farming can’t feed up these demands and we must have to use modern farming methods. Modern farming can also be a great source of employment and income. The unemployed people can start agribusiness. Thus we can make a strong economy and hunger free world. One of the major challenges we identified while working with farmers in India is an information gap and ease of access to that information. Each time we explained and questions kept streaming as to where to access inputs/services, how to plant or to control a certain farming problem.
           This is how Indian AgriFarm Forum came to be. A one stop farming information platform that offers ease of access to current farming information in a simplified and interactive way through the our discussion forum.
         </p>
+        <p>Agriculture plays a vital role in the Indian economy. Over 70 per cent of the rural households depend on agriculture. Agriculture is an important sector of Indian economy as it contributes about 17% to the total GDP and provides employment to over 60% of the population.</p>
 
       </div>
     </div>
